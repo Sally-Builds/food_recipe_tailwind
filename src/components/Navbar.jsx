05cbbe16dd5b2af5fa12caf85914d0472cc2ed40
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-05-03 00:25:07 +0100

    basic react with tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,177 @@
-import React from "react";
-
+import { useState } from "react";
+import {
+  ClockIcon,
+  HomeIcon,
+  QuestionMarkCircleIcon,
+  MailIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
   return (
-    <div>
-      <nav>
-        <div>
-          <h1>
-            <a href="/">Foof Ninja</a>
-          </h1>
+    <div className="text-gray-600 font-body">
+      <div className="grid md:grid-cols-3">
+        <div className="md:cols-span-1 md:flex md:justify-end">
+          <nav className="text-right">
+            <div className="flex justify-between items-center">
+              <h1 className="font-bold uppercase p-4 border-b border-gray-100">
+                <a
+                  href="/"
+                  className="text-green-500 md:text-blue-500 hover:text-gray-600"
+                >
+                  Foof Ninja
+                </a>
+              </h1>
+              <div className="md:hidden px-4 cursor-pointer">
+                <MenuIcon
+                  className="w-5"
+                  id="burger"
+                  onClick={handleNav}
+                ></MenuIcon>
+              </div>
+            </div>
+            <ul
+              id="menu"
+              className={nav ? "sm:block" : "hidden text-sm mt-6 md:block"}
+            >
+              <li>
+                <a
+                  href="/"
+                  className="px-4 flex justify-end border-r-4 border-primary"
+                >
+                  <span>Home</span>
+                  <HomeIcon className="w-5 ml-2"></HomeIcon>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="px-4 flex justify-end border-r-4 border-white"
+                >
+                  <span>About</span>
+                  <QuestionMarkCircleIcon className="w-5 ml-2"></QuestionMarkCircleIcon>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="px-4 flex justify-end border-r-4 border-white"
+                >
+                  <span>Contact</span>
+                  <MailIcon className="w-5 ml-2"></MailIcon>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <ul>
-          <li>
-            <a href="/">
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <span>About</span>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <span>Contact</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
 
-      <main>
-        <div>
-          <a href="/">Login</a>
-          <a href="/">Sign up</a>
-        </div>
+        <main className="px-16 py-6 bg-gray-100 md:col-span-2">
+          <div className="flex justify-center md:justify-end">
+            <a
+              href="/"
+              className="text-primary btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500"
+            >
+              Login
+            </a>
+            <a
+              href="/"
+              className="text-primary ml-2 btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500"
+            >
+              Sign up
+            </a>
+          </div>
 
-        <header>
-          <h2>Recipe</h2>
-          <h3>For Ninjas</h3>
-        </header>
-
-        <div>
-          <h4>Lateest Recipes</h4>
+          <header>
+            <h2 className="text-6xl">Recipe</h2>
+            <h3 className="text-3xl">For Ninjas</h3>
+          </header>
 
           <div>
-            <div>
-              <img src="" alt="" />
+            <h4 className="font-semibold mt-12 pb-2 border-b border-gray-200">
+              Lateest Recipes
+            </h4>
 
-              <div>
-                <span>5 Cooked Okpa</span>
-                <span>Recipe By Sally ecore</span>
+            <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+              <div className="card hover:shadow-xl">
+                <img
+                  src="stew.jpg"
+                  alt=""
+                  className="w-full h-32 sm:h-48 object-cover"
+                />
+
+                <div className="m-4">
+                  <span className="font-bold">5 Cooked Okpa</span>
+                  <span className="block text-gray-500 text-sm">
+                    Recipe By Sally ecore
+                  </span>
+                </div>
+
+                <div className="badge">
+                  <ClockIcon className="w-5 inline-block"></ClockIcon>
+                  <span>25 mins</span>
+                </div>
+              </div>
+
+              <div className="card hover:shadow-lg">
+                <img
+                  src="noodles.jpg"
+                  alt=""
+                  className="w-full h-32 sm:h-48 object-cover"
+                />
+
+                <div className="m-4">
+                  <span className="font-bold">Noodles</span>
+                  <span className="block text-gray-500 text-sm">
+                    Recipe By Sally ecore
+                  </span>
+                </div>
+
+                <div className="badge">
+                  <ClockIcon className="w-5 inline-block"></ClockIcon>
+                  <span>25 mins</span>
+                </div>
+              </div>
+
+              <div className="card hover:shadow-lg">
+                <img
+                  src="curry.jpg"
+                  alt=""
+                  className="w-full h-32 sm:h-48 object-cover"
+                />
+
+                <div className="m-4">
+                  <span className="font-bold">5 Tofu</span>
+                  <span className="block text-gray-500 text-sm">
+                    Recipe By Sally ecore
+                  </span>
+                </div>
+
+                <div className="badge">
+                  <ClockIcon className="w-5 inline-block"></ClockIcon>
+                  <span>25 mins</span>
+                </div>
+              </div>
+            </div>
+
+            <h4 className="font-semibold mt-12 pb-2 border-b border-gray-200">
+              Most Popular Recipes
+            </h4>
+
+            <div className="mt-8"></div>
+
+            <div className="flex justify-center">
+              <div
+                className="bg-secondary-100 text-secondary-200 btn 
+              hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition hover:ease-out duration-300"
+              >
+                Load More
               </div>
             </div>
           </div>
-
-          <h4>Most Popular Recipes</h4>
-
-          <div>
-            <div>
-              <img src="" alt="" />
-
-              <div>
-                <span>5 Cooked Okpa</span>
-                <span>Recipe By Sally ecore</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
     screens: {
       sm: "640px",
       md: "768px",
